59d61ed36de0ba1fa79725aa8f10870046501fe9
Corey <crejaud@gmail.com>
2016-04-16 16:53:34 -0400

    switched token to code

--- js/auth.js
+++ js/auth.js
@@ -180,7 +180,7 @@ function loginWithSpotify() {
     //}
 
     var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-        '&response_type=token' +
+        '&response_type=code' +
         '&scope=' + encodeURIComponent(scopes) +
         '&redirect_uri=' + encodeURIComponent(redirect_uri);
     document.location = url;
